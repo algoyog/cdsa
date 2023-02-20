6b30f3c74ce3c725098b03c43ce2fa78fce52353
Aravind B <aravindb@Aravinds-MacBook-Pro.local>
2023-02-20 21:02:35 +0530

    Updates

--- ds/main.c
+++ ds/main.c
@@ -4,8 +4,8 @@
 void testll();
 
 void removeDup();
-void recuRemoveDup();
-
+Node* findUniqNext(Node* inp);
+static int dupCount = 0;
 int main() {
     printf("Hello, World!\n");
     removeDup();
@@ -14,8 +14,33 @@ int main() {
 
 void removeDup() {
     int input[] = {1, 3, 3, 3, 4, 6, 6, 7, 8};
+    //int input[] = {2,4, 5, 16, 23, 34, 45, 51};
     Node *head = ll_from_array(input, ARRAY_SIZE(input));
-    //recuRemoveDup(head,head);
+    Node *temp = head;
+    do {
+        temp->next = findUniqNext(temp);
+        temp=temp->next;
+    } while (temp!=NULL);
+    printf("Hello");
+}
+
+Node* findUniqNext(Node* inp){
+    Node* next = inp->next;
+    if(next == NULL) return NULL;
+    Node* nextnext = next->next;
+    if(nextnext==NULL) return next;
+    int flag = -1;
+    while(next->data == nextnext->data){
+        flag = 1;
+        nextnext = nextnext->next;
+
+    }
+    if(flag == 1) {
+        dupCount++;
+        return nextnext;
+    }
+    else
+        return next;
 }
 
 
