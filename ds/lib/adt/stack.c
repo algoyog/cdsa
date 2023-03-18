96c8315ab46705418bc5cb0a48cd5ecf7625e47f
Aravind B <aravindb@Aravinds-MacBook-Pro.local>
2023-03-18 08:38:38 +0530

    Implement pop

--- ds/lib/adt/stack.c
+++ ds/lib/adt/stack.c
@@ -3,45 +3,56 @@
 //
 #include "../linkedlist/linkedlist.h"
 
-enum impl_backend{Array, LinkedList};
-const int  LIMIT = 100;
+enum impl_backend {
+    Array, LinkedList
+};
+const int LIMIT = 100;
 const float PERCENT_EXPAND = .8;
 
-typedef struct Stack{
-    enum  impl_backend type;
+typedef struct Stack {
+    enum impl_backend type;
     int len;
     int capacity;
-    int* input;
-    Node* head;
+    int *input;
+    Node *head;
 } Stack;
 
 Stack initialize(enum impl_backend);
-void push(Stack*, int);
-void printstack(Stack*);
 
-Stack initialize(enum impl_backend input){
+void push(Stack *, int);
+
+void printstack(Stack *);
+
+void pop(Stack *);
+
+Stack initialize(enum impl_backend input) {
     Stack stk;
-    stk.type=input;
-    stk.len=0;
-    stk.input=(int*) calloc(LIMIT, sizeof(int));
-    stk.capacity=LIMIT;
+    stk.type = input;
+    stk.len = 0;
+    stk.input = (int *) calloc(LIMIT, sizeof(int));
+    stk.capacity = LIMIT;
     return stk;
 }
 
-void push(Stack* ref, int element){
-    int curr_limit=PERCENT_EXPAND * ref->capacity;
-    if(ref->len>curr_limit){
-        ref->capacity = ref->capacity+LIMIT;
-        ref->input = realloc(ref->input,ref->capacity);
+void push(Stack *ref, int element) {
+    int curr_limit = PERCENT_EXPAND * ref->capacity;
+    if (ref->len > curr_limit) {
+        ref->capacity = ref->capacity + LIMIT;
+        ref->input = realloc(ref->input, ref->capacity);
     }
     ref->len++;
-    *(ref->input+ref->len) = element;
+    *(ref->input + ref->len) = element;
 }
 
-void printstack(Stack* ref){
+void pop(Stack *ref) {
+    ref->len--;
+    //TODO: Need to add logic to reduce the capacity
+}
+
+void printstack(Stack *ref) {
     int size = ref->len;
-    while (size>0){
-        printf("%d ",*(ref->input  +size));
+    while (size > 0) {
+        printf("%d ", *(ref->input + size));
         size--;
     }
 }
